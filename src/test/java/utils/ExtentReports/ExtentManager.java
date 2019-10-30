5460deb66425bddd89f798377cf5c33c205f2a88
Amir Hossein Montazerolghaem <amir.montazer@gmail.com>
2019-10-30 08:40:21 +0330

    Add extent report config file

--- src/test/java/utils/ExtentReports/ExtentManager.java
+++ src/test/java/utils/ExtentReports/ExtentManager.java
@@ -2,19 +2,18 @@ package utils.ExtentReports;
 
 import com.relevantcodes.extentreports.ExtentReports;
 
+import java.io.File;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-//OB: ExtentReports extent instance created here. That instance can be reachable by getReporter() method.
 public class ExtentManager {
 
     private static ExtentReports extent;
 
     public synchronized static ExtentReports getReporter() {
         if (extent == null) {
-            //Set HTML reporting file location
-//            String workingDir = System.getProperty("user.dir");
             extent = new ExtentReports("ExtentReports/" + getCurrentDateTime() + ".html", true);
+            extent.loadConfig(new File("resources/extent-config.xml"));
         }
         return extent;
     }
